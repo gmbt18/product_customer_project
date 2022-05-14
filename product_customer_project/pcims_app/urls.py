1eb6e012b56bd7ef99acbde6db63c98b9a65009c
nrgdcbg <reyes.allen13@gmail.com>
2022-05-14 09:22:25 +0800

    added pcims_app

--- product_customer_project/pcims_app/urls.py
+++ product_customer_project/pcims_app/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-    path('', views.home, name="productHome"),
+    path('', views.index, name='index'),
+    path('products', views.products, name='productsPage'),
+    path('login', views.login, name='loginPage'),
+    path('change-password', views.changePassword, name='changePassword'),
+    path('suppliers', views.suppliers, name='suppliersPage'),
+    path('suppliers/products', views.supplierProducts, name='supplierProductsPage'),
+    path('products/sample', views.productReviews, name='productReviewsPage'),
+    path('myreviews', views.myReviews, name='myReviewsPage'),
 ]
--- product_customer_project/pcims_app/views.py
+++ product_customer_project/pcims_app/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
-def home(request):
-    return render(request, 'pcims_app/home.html')
+def index(request):
+    return render(request, 'pcims_app/dashboard.html')
+
+def products(request):
+    return render(request, 'pcims_app/products.html')
+
+def login(request):
+    return render(request, 'pcims_app/login.html')
+
+def changePassword(request):
+    return render(request, 'pcims_app/changepassword.html')
+
+def suppliers(request):
+    return render(request, 'pcims_app/suppliers.html')
+   
+def supplierProducts(request):
+    return render(request, 'pcims_app/supplierProducts.html')
+
+def productReviews(request):
+    return render(request, 'pcims_app/productsample.html')
+
+def myReviews(request):
+    return render(request, 'pcims_app/myreviews.html')
