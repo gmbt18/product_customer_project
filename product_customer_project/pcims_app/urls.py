ea8d718c839b25dbe942439954c135f787d11472
nrgdcbg <reyes.allen13@gmail.com>
2022-05-25 09:16:23 +0800

    forgotpass

--- product_customer_project/pcims_app/urls.py
+++ product_customer_project/pcims_app/urls.py
@@ -3,6 +3,8 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     path('', views.indexPage, name='indexPage'),
     path('dashboard/', views.dashboardPage, name='dashboardPage'),
@@ -45,5 +47,14 @@ urlpatterns = [
     path('profile/change-password', views.profileChangePassPage, name='profileChangePassPage'),
     path('myreviews', views.myReviewsPage, name='myReviewsPage'),
     path('myreviews/<int:id>/edit', views.reviewsEditPage, name='reviewsEditPage'),
+
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name = 'reset_password'),
+
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name = 'password_reset_done'),
+
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name = 'password_reset_confirm'),
+
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name = 'password_reset_complete'),
 ]
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
